c53dee44700f74b0a70befdf30291aecc00b0d6a
Eroshchuk Anton <eroschuk@gmail.com>
2022-04-27 04:06:37 +0300

    Updated pom.xml and Lion class

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Lion{
 
     public List<String> eatMeat() throws Exception {
-        return getFood("Хищник");
+        return List.of("Животные", "Птицы", "Рыба");
     }
 
     boolean hasMane;
@@ -36,13 +36,4 @@ public class Lion{
         return "Кошачьи";
     }
 
-    public List<String> getFood(String animalKind) throws Exception {
-        if ("Травоядное".equals(animalKind)) {
-            return List.of("Трава", "Различные растения");
-        } else if ("Хищник".equals(animalKind)) {
-            return List.of("Животные", "Птицы", "Рыба");
-        } else {
-            throw new Exception("Неизвестный вид животного, используйте значение Травоядное или Хищник");
-        }
-    }
 }
